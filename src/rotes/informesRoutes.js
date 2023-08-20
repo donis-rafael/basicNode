7ece1f4bc8de62a9b96e1f2694d5498090965658
Rafael Morales <rafael.7.morales@gmail.com>
2023-08-19 21:58:50 -0600

    cors

--- src/rotes/informesRoutes.js
+++ src/rotes/informesRoutes.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 
 const reportes_controller = require('../controllers/informesController');
 
-router.get('/diario', reportes_controller.getReporteDiario);
-router.get('/semanal', reportes_controller.getReporteSemanal);
-router.get('/mensual', reportes_controller.getReporteMensual);
+router.get('/diario', reportes_controller.getInformeDiario);
+router.get('/semanal', reportes_controller.getInformeSemanal);
+router.get('/mensual', reportes_controller.getInformeMensual);
 
 module.exports = router;
