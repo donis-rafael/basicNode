12251e6fcf9475fd7c280096e02a5ecd748d5fd5
Rafael Morales <rafael.7.morales@gmail.com>
2023-08-19 21:52:39 -0600

    cors

--- src/app/app.js
+++ src/app/app.js
@@ -4,16 +4,18 @@ var logger = require('morgan');
 
 const app = express();
 
-app.use(cors());
 app.use(logger('dev'));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors({
+    origin: '*'
+}));
 
 var routes_auth = require('../rotes/authRoutes');
 var routes_admin = require('../rotes/adminRoutes');
 var routes_app = require('../rotes/appRoutes');
-var routes_reportes = require('../rotes/reportesRoutes');
+var routes_informes = require('../rotes/informesRoutes');
 var routes_consultas = require('../rotes/consultasRoutes');
 
 app.get('/', (req, res) => {
@@ -22,7 +24,7 @@ app.get('/', (req, res) => {
 app.use('/api/auth', routes_auth);
 app.use('/api/admin', routes_admin);
 app.use('/api/app-movil', routes_app);
-app.use('/api/reportes', routes_reportes);
+app.use('/api/informes', routes_informes);
 app.use('/api/consultas', routes_consultas);
 
 module.exports = app;
