da1f74e5458d8e0dfd992823bd2f7fc956c6dac0
Rafael Morales <rafael.7.morales@gmail.com>
2023-08-22 23:44:57 -0600

    ajustes app movil

--- src/routes/informesRoutes.js
+++ src/routes/informesRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const reportes_controller = require('../controllers/informesController');
 
-router.get('/diario', reportes_controller.getInformeDiario);
+router.get('/diario', reportes_controller.getInformeDiario_Maquina);
 router.get('/semanal', reportes_controller.getInformeSemanal);
 router.get('/mensual', reportes_controller.getInformeMensual);
 
