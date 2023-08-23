a4eba3e7ec1422348e69a05abc541c0f0fb224b2
Rafael Morales <rafael.7.morales@gmail.com>
2023-08-22 20:13:15 -0600

    cors

--- src/app/app.js
+++ src/app/app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
-app.use(function (req, res, next) {
+/*app.use(function (req, res, next) {
     // res.header("Access-Control-Allow-Origin", "*");
     const allowedOrigins = ['http://localhost:3000', 'http://190.148.50.160:3000'];
     const origin = req.headers.origin;
@@ -16,7 +16,7 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-credentials", true);
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
     next();
-});
+});*/
 
 app.use(logger('dev'));
 
