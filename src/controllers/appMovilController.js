a28770bab6bf3fc6405c5377a1b1dca777f03540
Rafael Morales <rafael.7.morales@gmail.com>
2023-12-19 19:23:05 -0600

    log

--- src/controllers/appMovilController.js
+++ src/controllers/appMovilController.js
@@ -23,7 +23,7 @@ controller.getIngenios = async (req, res) => {
     // VALIDAR TOKEN
     let cadenaCompara = helper.cadenaCompara(user);
     let cadenaComparaEncripted = await helper.encryptPassword(cadenaCompara);
-    log(cadenaComparaEncripted);
+    console.log(cadenaComparaEncripted);
 
     const match = await helper.comparaPassword(cadenaCompara, token);
 
