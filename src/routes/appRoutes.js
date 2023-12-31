6a669fe7fad8b26f013adb1ff95084a0336186d3
Rafael Morales <rafael.7.morales@gmail.com>
2023-12-30 20:43:04 -0600

    pruebas

--- src/routes/appRoutes.js
+++ src/routes/appRoutes.js
@@ -5,7 +5,7 @@ const app_movil_controller = require('../controllers/appMovilController');
 // GETS
 router.get('/:user/clientes', app_movil_controller.getIngenios);
 router.get('/:user/fincas', app_movil_controller.getFincas);
-router.get('/:user/frentes', app_movil_controller.getFrentes);
+router.get('/:user/:ingenio/frentes', app_movil_controller.getFrentes);
 router.get('/:user/maquinas', app_movil_controller.getMaquinas);
 router.get('/:user/tipoMantenimiento', app_movil_controller.getTipoMantenimiento);
 router.get('/:user/usuariosApp', app_movil_controller.getUsuariosAppMovil);
